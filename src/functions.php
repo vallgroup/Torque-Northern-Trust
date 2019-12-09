842941861321e3261f7a087bc71afbd5b63bc4d5
Mario Vallejo <mario@vallgroup.com>
2019-12-09 13:32:01 -0600

    show users menu in the backend

--- src/functions.php
+++ src/functions.php
@@ -91,7 +91,7 @@ if ( class_exists( 'Torque_Map_CPT' ) ) {
    remove_menu_page( 'edit-comments.php' );          //Comments
    // remove_menu_page( 'themes.php' );                 //Appearance
    //remove_menu_page( 'plugins.php' );                //Plugins
-   remove_menu_page( 'users.php' );                  //Users
+   // remove_menu_page( 'users.php' );                  //Users
    remove_menu_page( 'tools.php' );                  //Tools
    // remove_menu_page( 'options-general.php' );        //Settings
 
